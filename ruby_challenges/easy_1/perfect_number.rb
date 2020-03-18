ff87cb355f7a761ea2b047fa0a7b2baec0c9bf57
Josephine Xu <lilmaifun@gmail.com>
2020-03-18 15:27:43 -0700

    perfect numbers

---
+++ ruby_challenges/easy_1/perfect_number.rb
@@ -0,0 +1,20 @@
+class PerfectNumber
+  def self.classify(num)
+    raise RuntimeError if num < 0
+
+    divisors = []
+    1.upto(num) do |n|
+      divisors << n if num % n == 0 unless num == n
+    end
+
+    if num == divisors.reduce(:+)
+      "perfect"
+    elsif num > divisors.reduce(:+)
+      "abundant"
+    else num < divisors.reduce(:+)
+      "deficient"
+    end
+  end
+end
+
+p PerfectNumber.classify(6)
